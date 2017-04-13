888e38b6ec8ffca97d6a600267899467a13f03b7
Aaron Barber <abarberaus@gmail.com>
2017-04-13 12:21:01 +0800

    template load

---
+++ pricing/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $("#priceTable").owlCarousel({
+        items: 4,
+        navigation : true, // Show next and prev buttons
+	    slideSpeed : 400,
+	    paginationSpeed : 400,
+	    navigationText : false,
+	    autoPlay: true,
+	    pagination: false
+    });
+});
